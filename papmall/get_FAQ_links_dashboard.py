3f91b83c6a7d682a6ba4e325dcde4e7e49d72e89
Nguyễn Thanh Nguyên <nguyenkiddo0@gmail.com>
2023-05-05 22:24:19 +0700

    update function get link FAQs and dashboard links

---
+++ papmall/get_FAQ_links_dashboard.py
@@ -0,0 +1,29 @@
+# get dashboard + faq links
+from login_auto import *
+
+def get_faqs_title_and_slug(selector, num_of_faqs):
+    cell_title_array = []
+    cell_title_slug_array = []
+    for i in range(num_of_faqs):
+        cell_item = selector[i].text
+        cell_item_array = cell_item.split("\n")
+        cell_title = cell_item_array[0]
+        cell_title_slug = cell_item_array[1]
+        cell_title_array.append(cell_title)
+        cell_title_slug_array.append(cell_title_slug)
+    cell_title_array_result = cell_title_array[::-1]
+    cell_title_slug_array_result = cell_title_slug_array[::-1]
+    return [cell_title_array_result, cell_title_slug_array_result]
+
+def get_faqs_links(num_of_faqs):
+    cell_array = driver.find_elements(By.CSS_SELECTOR,".bootstrap-datatable > tbody > tr > td:nth-child(2)")
+    title_arr, sug_title_arr = get_faqs_title_and_slug(cell_array, num_of_faqs)
+    sug_title_str = '\n'.join(sug_title_arr)
+    inputArray = sug_title_str.split("\n")
+    for value in inputArray:
+        print(f"https://www.paycec.com/faq/{value}")
+def get_dashboard_links(num_of_faqs):
+    # get all dashboard link faq
+    edit_btn = driver.find_elements(By.CSS_SELECTOR,".btn.btn-info")
+    for i in range(num_of_faqs - 1, 0, -1):
+        print(edit_btn[i].get_attribute("href"))
--- papmall/main.py
+++ papmall/main.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 from docs_to_html import *
 from login_auto import *
-
+from get_FAQ_links_dashboard import *
+import requests
 
 def main():
     # Load the environment variables from .env file
@@ -30,43 +31,9 @@ def main():
     # faq_type = "Amex"
     # for i in range(num_of_faqs):
     #     input_faq(faq_name[i], faq_content[i], faq_type)
-    
     num_of_faqs = 7
-    # get dashboard + faq links
-    cell_array = driver.find_elements(By.CSS_SELECTOR,".bootstrap-datatable > tbody > tr > td:nth-child(2)")
-    def get_faqs_title_and_slug(selector):
-        cell_title_array = []
-        cell_title_slug_array = []
-        for i in range(num_of_faqs):
-            cell_item = selector[i].text
-            cell_item_array = cell_item.split("\n")
-            cell_title = cell_item_array[0]
-            cell_title_slug = cell_item_array[1]
-            cell_title_array.append(cell_title)
-            cell_title_slug_array.append(cell_title_slug)
-        cell_title_array_result = cell_title_array[::-1]
-        cell_title_slug_array_result = cell_title_slug_array[::-1]
-        return [cell_title_array_result, cell_title_slug_array_result]
-
-    title_arr, sug_title_arr = get_faqs_title_and_slug(cell_array)
-    # Make a GET request to the webpage
-    response = requests.get(dashboard_url)
-    # Create a BeautifulSoup object from the HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-    for item in sug_title_arr:
-        print(item)
-    for item in sug_title_arr:
-        print(item)
-    sug_title_str = '\n'.join(sug_title_arr)
-    print(sug_title_str)
-    inputArray = sug_title_str.split("\n")
-    for value in inputArray:
-        print(f"https://www.paycec.com/faq/{value}")
-    # get all dashboard link faq
-    btn = soup.select(".btn.btn-info")
-    for i in range(num_of_faqs - 1, -1, -1):
-        print(btn[i]['href'])
-
+    get_faqs_links(num_of_faqs)
+    get_dashboard_links(num_of_faqs)
 
 
 if __name__ == "__main__":
